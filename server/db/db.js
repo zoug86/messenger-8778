541f6119d9f5d90f26b9576c3e82979fa802b65e
Houssem Marzougui <marzouguih86@gmail.com>
2021-09-30 01:19:45 -0700

    fixed the scrollbottom and message display

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:123123h@localhost/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost/${process.env.POSTGRES_DATABASE}`, {
   logging: false
 });
 
