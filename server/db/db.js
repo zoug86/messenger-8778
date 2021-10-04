0fcde70c3f9d3b659360812efd3ae7172e4dba81
Houssem Marzougui <marzouguih86@gmail.com>
2021-10-04 01:37:18 -0700

    added credentials to env

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:123123h@localhost/messenger`, {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost/${process.env.POSTGRES_DATABASE}`, {
   logging: false
 });
 
 module.exports = db;
-
-//`postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost/${process.env.POSTGRES_DATABASE}`
