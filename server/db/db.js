fde189016c50aacd79a2487ad9277c5cada81877
Houssem Marzougui <marzouguih86@gmail.com>
2021-10-04 01:16:15 -0700

    fixed state

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost/${process.env.POSTGRES_DATABASE}`, {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:123123h@localhost/messenger`, {
   logging: false
 });
 
 module.exports = db;
+
+//`postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost/${process.env.POSTGRES_DATABASE}`
